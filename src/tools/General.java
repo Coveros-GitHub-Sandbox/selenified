f463ee093b5f03c48f5c7949f0f45696f0cf971e
Max Saperstone <max.saperstone@coveros.com>
2016-05-10 11:14:56 -0400

    Adding in proxy capability

--- src/tools/General.java
+++ src/tools/General.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
