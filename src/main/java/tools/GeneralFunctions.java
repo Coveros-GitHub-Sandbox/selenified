d11da171428e3e4ff9f63c95fa149e1554df7e24
Max Saperstone <max.saperstone@coveros.com>
2017-02-21 10:57:34 -0500

    Adding in license to files, and fixing some packaging names

--- src/main/java/tools/GeneralFunctions.java
+++ src/main/java/tools/GeneralFunctions.java
@@ -1,25 +1,46 @@
-package tools;
+/*
+ * Copyright 2017 Coveros, Inc.
+ * 
+ * This file is part of Selenified.
+ * 
+ * Selenified is licensed under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy 
+ * of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on 
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+ * KIND, either express or implied. See the License for the 
+ * specific language governing permissions and limitations 
+ * under the License.
+ */
+
+package main.java.tools;
 
 import static org.testng.AssertJUnit.assertEquals;
 
 import java.io.IOException;
 
-import tools.logging.TestOutput;
+import main.java.tools.logging.TestOutput;
 
 public class GeneralFunctions {
 
-    /**
-     * Closes out the output file and checks the error count to see how the
-     * tests went, overall passed or failed
-     * 
-     * @param output
-     *            - the TestOutput class that is instantiated
-     * @throws IOException
-     *             - an IOException
-     */
-    public void stopTest(TestOutput output) throws IOException {
-        stopTest(output, output.getErrors());
-    }
+	/**
+	 * Closes out the output file and checks the error count to see how the
+	 * tests went, overall passed or failed
+	 * 
+	 * @param output
+	 *            - the TestOutput class that is instantiated
+	 * @throws IOException
+	 *             - an IOException
+	 */
+	public void stopTest(TestOutput output) throws IOException {
+		stopTest(output, output.getErrors());
+	}
+
 	/**
 	 * Closes out the output file and checks the error count to see how the
 	 * tests went, overall passed or failed
@@ -32,7 +53,7 @@ public class GeneralFunctions {
 	 *             - an IOException
 	 * @deprecated use {@link #finalize()} instead
 	 */
-    @Deprecated
+	@Deprecated
 	public void stopTest(TestOutput output, int errors) throws IOException {
 		output.endTestTemplateOutputFile();
 		// new PDFOutput( output.getOutputFileName() );
