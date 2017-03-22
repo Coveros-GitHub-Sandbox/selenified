b48dfd3496f6cb2e47d94a2647d88e0b2bcc68ad
Max Saperstone <max.saperstone@coveros.com>
2017-03-22 16:32:55 -0400

    Cleaning up General un-used imports; Pulling selenium setup methods into separate class

--- src/main/java/tools/General.java
+++ src/main/java/tools/General.java
@@ -20,18 +20,9 @@
 
 package tools;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
