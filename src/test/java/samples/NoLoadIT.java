7cb644d716b7b0e3a40ad9da2d507ad042550471
Max Saperstone <max.saperstone@coveros.com>
2017-05-17 18:35:07 -0400

    Fixing check for no-load

--- src/test/java/samples/NoLoadIT.java
+++ src/test/java/samples/NoLoadIT.java
@@ -46,7 +46,7 @@ public class NoLoadIT extends TestBase {
 				asserts.getOutputFile().countInstancesOf("Opening new browser and loading up starting page"), 0);
 		// verify the page wasn't attempted to load
 		asserts.compareURL("http://172.31.2.65/");
-		// verify no issues
+		// verify one issue from the above check
 		finish(1);
 	}
 }
