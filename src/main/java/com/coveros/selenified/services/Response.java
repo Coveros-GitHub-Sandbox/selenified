75617f2f0625a4e5e584900b66071a5f453cbf8f
Max Saperstone <max.saperstone@coveros.com>
2017-08-04 10:06:27 -0400

    Refactoring asserts for services

--- src/main/java/com/coveros/selenified/services/Response.java
+++ src/main/java/com/coveros/selenified/services/Response.java
@@ -20,7 +20,12 @@
 
 package com.coveros.selenified.services;
 
+import java.util.Map;
+
+import com.coveros.selenified.output.OutputFile;
+import com.coveros.selenified.output.Assert.Success;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 /**
@@ -35,6 +40,17 @@ public class Response {
     private JsonObject object = null;
     private JsonArray array = null;
     private String message;
+
+    // this will be the name of the file we write all commands out to
+    private OutputFile file;
+
+    // constants
+    private static final String FOUND = "Found a response of:";
+
+    // a basic response setup, just with an output file to write information to
+    public Response(OutputFile file) {
+        this.file = file;
+    }
 
     public Response(int code) {
         this.code = code;
@@ -86,5 +102,143 @@ public class Response {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public void setOutputFile(OutputFile file) {
+        this.file = file;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    // some comparisons for our services
+    ///////////////////////////////////////////////////////////////////
+
+    /**
+     * compares actual response code to the expected response code, and write
+     * that out to the output file
+     * 
+     * @param expectedCode
+     *            - the expected response code
+     * @return Integer: 1 if a failure and 0 if a pass
+     */
+    public int assertEquals(int expectedCode) {
+        Success success = (code == expectedCode) ? Success.PASS : Success.FAIL;
+        file.recordExpected("Expected to find a response code of <b>" + expectedCode + "</b>");
+        file.recordActual("Found a response code of <b>" + code + "</b>", success);
+        file.addErrors(success.getErrors());
+        return success.getErrors();
+    }
+
+    /**
+     * compares actual response json payload to the expected response json
+     * payload, and write that out to the output file
+     * 
+     * @param expectedJson
+     *            - the expected response json object
+     * @return Integer: 1 if a failure and 0 if a pass
+     */
+    public int assertEquals(JsonObject expectedJson) {
+        Success success = Success.FAIL;
+        if (object != null) {
+            success = object.equals(expectedJson) ? Success.PASS : Success.FAIL;
+        }
+        file.recordExpected(
+                "Expected to find a response of:" + file.formatResponse(new Response(0, expectedJson, null)));
+        file.recordActual(FOUND + file.formatResponse(this), success);
+        file.addErrors(success.getErrors());
+        return success.getErrors();
+    }
+
+    /**
+     * compares actual response json payload to the expected response json
+     * payload, and write that out to the output file
+     * 
+     * @param expectedArray
+     *            - the expected response json array
+     * @return Integer: 1 if a failure and 0 if a pass
+     */
+    public int assertEquals(JsonArray expectedArray) {
+        Success success = Success.FAIL;
+        if (array != null) {
+            success = array.equals(expectedArray) ? Success.PASS : Success.FAIL;
+        }
+        file.recordExpected(
+                "Expected to find a response of:" + file.formatResponse(new Response(0, expectedArray, null)));
+        file.recordActual(FOUND + file.formatResponse(this), success);
+        file.addErrors(success.getErrors());
+        return success.getErrors();
+    }
+
+    /**
+     * checks to see if actual response json payload to the expected json
+     * element, and write that out to the output file
+     * 
+     * @param expectedPairs
+     *            a hashmap with string key value pairs expected in the json
+     *            response
+     * @return Integer: 1 if a failure and 0 if a pass
+     */
+    public int assertContains(Map<String, String> expectedPairs) {
+        StringBuilder expectedString = new StringBuilder();
+        Success success = Success.FAIL;
+        if (object != null) {
+            success = Success.PASS;
+            for (Map.Entry<String, String> entry : expectedPairs.entrySet()) {
+                expectedString.append("<div>");
+                expectedString.append(entry.getKey());
+                expectedString.append(" : ");
+                expectedString.append(entry.getValue());
+                expectedString.append("</div>");
+                if (!object.has(entry.getKey()) || !object.get(entry.getKey()).getAsString().equals(entry.getValue())) {
+                    success = Success.FAIL;
+                }
+            }
+        }
+        file.recordExpected(
+                "Expected to find a response containing: <div><i>" + expectedString.toString() + "</i></div>");
+        file.recordActual(FOUND + file.formatResponse(this), success);
+        file.addErrors(success.getErrors());
+        return success.getErrors();
+    }
+
+    /**
+     * checks to see if actual response json payload to the expected json
+     * element, and write that out to the output file
+     * 
+     * @param key
+     *            - a String key value expected in the result
+     * @param expectedJson
+     *            - the expected response json object
+     * @return Integer: 1 if a failure and 0 if a pass
+     */
+    public int assertContains(String key, JsonElement expectedJson) {
+        Success success = Success.FAIL;
+        if (object != null && object.has(key)) {
+            success = object.get(key).equals(expectedJson) ? Success.PASS : Success.FAIL;
+        }
+        file.recordExpected("Expected to find a response containing: "
+                + file.formatResponse(new Response(0, expectedJson.getAsJsonObject(), null)));
+        file.recordActual(FOUND + file.formatResponse(this), success);
+        file.addErrors(success.getErrors());
+        return success.getErrors();
+    }
+
+    /**
+     * checks to see if actual response json payload to the expected json
+     * element, and write that out to the output file
+     * 
+     * @param expectedJson
+     *            - the expected response json array
+     * @return Integer: 1 if a failure and 0 if a pass
+     */
+    public int assertContains(JsonElement expectedJson) {
+        Success success = Success.FAIL;
+        if (array != null) {
+            success = array.contains(expectedJson) ? Success.PASS : Success.FAIL;
+        }
+        file.recordExpected("Expected to find a response containing:"
+                + file.formatResponse(new Response(0, expectedJson.getAsJsonObject(), null)));
+        file.recordActual(FOUND + file.formatResponse(this), success);
+        file.addErrors(success.getErrors());
+        return success.getErrors();
     }
 }
