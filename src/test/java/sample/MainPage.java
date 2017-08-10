3e9d2b218efe77c24dddcac048e354d3ab450c29
Max Saperstone <max.saperstone@coveros.com>
2017-08-10 11:27:54 -0400

    Adding in POM sample tests

---
+++ src/test/java/sample/MainPage.java
@@ -0,0 +1,34 @@
+package sample;
+
+import com.coveros.selenified.selenium.App;
+import com.coveros.selenified.selenium.Selenium.Locator;
+import com.coveros.selenified.selenium.element.Element;
+
+public final class MainPage {
+
+	// our page elements
+	private Element click;
+	private Element alert;
+	private Element carList;
+	public Element checkbox;
+	
+	public MainPage(App app) {
+		click = app.newElement(Locator.CLASSNAME, "click");
+		alert = app.newElement(Locator.CSS, "input#alert_button");
+		carList = app.newElement(Locator.ID, "car_list");
+		checkbox = app.newElement(Locator.XPATH, "//form/input[@type='checkbox']");
+	}
+	
+	public void selectCar(String car) {
+		carList.select(car);
+	}
+	
+	public void assertCar(String car) {
+		carList.assertEquals().selectedOption(car);
+	}
+	
+	public void generateAlert() {
+		click.click();
+		alert.click();
+	}	
+}
---
+++ src/test/java/sample/POMSampleIT.java
@@ -0,0 +1,74 @@
+package sample;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.coveros.selenified.selenium.App;
+import com.coveros.selenified.tools.Selenified;
+
+public class POMSampleIT extends Selenified {
+
+	@BeforeClass(alwaysRun = true)
+	public void beforeClass() {
+		// set the base URL for the tests here
+		setTestSite("http://172.31.2.65/");
+		// set the author of the tests here
+		setAuthor("Max Saperstone\n<br/>max.saperstone@coveros.com");
+		// set the version of the tests or of the software, possibly with a
+		// dynamic check
+		setVersion("0.0.1");
+	}
+
+	@DataProvider(name = "car list items", parallel = true)
+	public Object[][] DataSetOptions() {
+		return new Object[][] { new Object[] { "Volvo" }, new Object[] { "Saab" }, new Object[] { "Mercedes" } };
+	}
+
+	ThreadLocal<MainPage> main = new ThreadLocal<>();;
+
+	@BeforeMethod(alwaysRun = true)
+	public void setupApp() {
+		main.set(new MainPage(this.apps.get()));
+	}
+
+	@Test(dataProvider = "car list items", groups = { "sample", "pom",
+			"virtual" }, description = "A sample test using a data provider to perform searches")
+	public void sampleTestWDataProvider(String listItem) {
+		// our test actions - use our threadsafe main object
+		main.get().selectCar(listItem);
+		main.get().assertCar(listItem);
+		// close out the test
+		finish();
+	}
+
+	@Test(groups = { "sample", "pom" }, description = "A sample test using a data provider to perform searches")
+	public void sampleTest() {
+		// grab our main app object
+		App app = this.apps.get();
+		// grab our main threadsafe object for future use
+		MainPage main = this.main.get();
+		// our test actions
+		main.generateAlert();
+		app.acceptAlert();
+		app.alertNotPresent();
+		// close out the test
+		finish();
+	}
+
+	@Test(groups = { "sample", "pom",
+			"virtual" }, description = "A sample test using a data provider to perform searches")
+	public void sampleTestWMatches() {
+		// define a new main object
+		MainPage main = new MainPage(this.apps.get());
+		// perform some actions
+		for (int match = 0; match < main.checkbox.get().matchCount(); match++) {
+			main.checkbox.setMatch(match);
+			main.checkbox.click();
+			main.checkbox.assertState().checked();
+		}
+		// close out the test
+		finish();
+	}
+}
