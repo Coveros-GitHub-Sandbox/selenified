2e11f6dca49a9b27c08ea121193470626eee5432
Max Saperstone <max.saperstone@coveros.com>
2018-04-30 15:49:54 -0400

    Adding fix for setting browser options via cmd

--- src/main/java/com/coveros/selenified/utilities/TestSetup.java
+++ src/main/java/com/coveros/selenified/utilities/TestSetup.java
@@ -58,6 +58,7 @@ public class TestSetup {
     // constants
     private static final String PROXY_INPUT = "proxy";
     private static final String BROWSER_INPUT = "browser";
+    private static final String BROWSER_OPTIONS = "options";
     private static final String HEADLESS_INPUT = "headless";
     private static final String BROWSER_NAME_INPUT = "browserName";
     private static final String BROWSER_VERSION_INPUT = "browserVersion";
@@ -254,7 +255,8 @@ public class TestSetup {
             case FIREFOX:
                 FirefoxDriverManager.getInstance().forceCache().setup();
                 FirefoxOptions firefoxOptions = new FirefoxOptions(capabilities);
-                if (System.getProperty(HEADLESS_INPUT) != null && "true".equals(System.getProperty(HEADLESS_INPUT))) {
+                firefoxOptions.addArguments(getBrowserOptions(browser));
+                if (runHeadless()) {
                     firefoxOptions.setHeadless(true);
                 }
                 driver = new FirefoxDriver(firefoxOptions);
@@ -263,7 +265,8 @@ public class TestSetup {
                 ChromeDriverManager.getInstance().forceCache().setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions = chromeOptions.merge(capabilities);
-                if (System.getProperty(HEADLESS_INPUT) != null && "true".equals(System.getProperty(HEADLESS_INPUT))) {
+                chromeOptions.addArguments(getBrowserOptions(browser));
+                if (runHeadless()) {
                     chromeOptions.setHeadless(true);
                 }
                 driver = new ChromeDriver(chromeOptions);
@@ -296,6 +299,26 @@ public class TestSetup {
                 throw new InvalidBrowserException("The selected browser " + browser + " is not an applicable choice");
         }
         return driver;
+    }
+
+    public static Boolean runHeadless() {
+        return System.getProperty(HEADLESS_INPUT) != null && "true".equals(System.getProperty(HEADLESS_INPUT));
+    }
+
+    public static List<String> getBrowserOptions(Browser browser) {
+        ArrayList<String> browserOptions = new ArrayList<>();
+        if (System.getProperty(BROWSER_OPTIONS) != null) {
+            browserOptions = new ArrayList(Arrays.asList(System.getProperty(BROWSER_OPTIONS).split("\\s*,\\s*")));
+        }
+        if (browser == Browser.CHROME && browserOptions.contains("--headless")) {
+            browserOptions.remove("--headless");
+            System.setProperty("headless", "true");
+        }
+        if (browser == Browser.FIREFOX && browserOptions.contains("-headless")) {
+            browserOptions.remove("-headless");
+            System.setProperty("headless", "true");
+        }
+        return browserOptions;
     }
 
     /**
