92d56b5f3c5fb17417e11452134f280bc44852cb
Max Saperstone <max.saperstone@coveros.com>
2018-05-14 12:20:43 -0400

    Adding initial annotation support for JIRA

---
+++ src/main/java/com/coveros/selenified/utilities/JiraAnnotation.java
@@ -0,0 +1,67 @@
+package com.coveros.selenified.utilities;
+
+import java.lang.reflect.Method;
+
+public class JiraAnnotation {
+
+    Method method;
+
+    public JiraAnnotation(Method method) {
+        this.method = method;
+    }
+
+    public Boolean isAnnotationPresent() {
+        if (method.getDeclaringClass().isAnnotationPresent(Jira.class)) {
+            return true;
+        }
+        return method.isAnnotationPresent(Jira.class);
+    }
+
+    public Boolean isProjectPresent() {
+        if (!isAnnotationPresent()) {
+            return false;
+        }
+        if (method.getDeclaringClass().isAnnotationPresent(Jira.class)) {
+            if (!"".equals(method.getDeclaringClass().getAnnotation(Jira.class).project())) {
+                return true;
+            }
+        }
+        return !"".equals(method.getAnnotation(Jira.class).project());
+    }
+
+    public Boolean isIssuePresent() {
+        if (!isAnnotationPresent()) {
+            return false;
+        }
+        if (method.getDeclaringClass().isAnnotationPresent(Jira.class)) {
+            if (!"".equals(method.getDeclaringClass().getAnnotation(Jira.class).issue())) {
+                return true;
+            }
+        }
+        return !"".equals(method.getAnnotation(Jira.class).issue());
+    }
+
+    public String getProject() {
+        if (!isProjectPresent()) {
+            return null;
+        }
+        if (method.isAnnotationPresent(Jira.class)) {
+            if (!"".equals(method.getAnnotation(Jira.class).project())) {
+                return method.getAnnotation(Jira.class).project();
+            }
+        }
+        return method.getDeclaringClass().getAnnotation(Jira.class).project();
+    }
+
+    public String getIssue() {
+        if (!isIssuePresent()) {
+            return null;
+        }
+        if (method.isAnnotationPresent(Jira.class)) {
+            if (!"".equals(method.getAnnotation(Jira.class).issue())) {
+                return method.getAnnotation(Jira.class).issue();
+            }
+        }
+        return method.getDeclaringClass().getAnnotation(Jira.class).issue();
+    }
+}
---
+++ src/test/java/unit/JiraTest.java
@@ -0,0 +1,58 @@
+package unit;
+
+import com.coveros.selenified.utilities.Jira;
+import com.coveros.selenified.utilities.JiraAnnotation;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+
+public class JiraTest {
+
+    @Test
+    public void verifyDefaultTest(Method method) {
+        Assert.assertFalse(new JiraAnnotation(method).isAnnotationPresent());
+    }
+
+    @Test
+    @Jira
+    public void verifyProjectTest(Method method) {
+        Assert.assertTrue(new JiraAnnotation(method).isAnnotationPresent());
+        Assert.assertFalse(new JiraAnnotation(method).isProjectPresent());
+        Assert.assertNull(new JiraAnnotation(method).getProject());
+    }
+
+    @Test
+    @Jira(project = "HW")
+    public void verifySetProjectTest(Method method) {
+        Assert.assertTrue(new JiraAnnotation(method).isAnnotationPresent());
+        Assert.assertTrue(new JiraAnnotation(method).isProjectPresent());
+        Assert.assertEquals(new JiraAnnotation(method).getProject(), "HW");
+    }
+
+    @Test
+    @Jira
+    public void verifyIssueTest(Method method) {
+        Assert.assertTrue(new JiraAnnotation(method).isAnnotationPresent());
+        Assert.assertFalse(new JiraAnnotation(method).isIssuePresent());
+        Assert.assertNull(new JiraAnnotation(method).getIssue());
+    }
+
+    @Test
+    @Jira(issue = "HW-123456")
+    public void verifySetIssueTest(Method method) {
+        Assert.assertTrue(new JiraAnnotation(method).isAnnotationPresent());
+        Assert.assertTrue(new JiraAnnotation(method).isIssuePresent());
+        Assert.assertEquals(new JiraAnnotation(method).getIssue(), "HW-123456");
+    }
+
+    @Test
+    @Jira(project = "HW", issue = "HW-123456")
+    public void verifySetProjectAndIssueTest(Method method) {
+        Assert.assertTrue(new JiraAnnotation(method).isAnnotationPresent());
+        Assert.assertTrue(new JiraAnnotation(method).isProjectPresent());
+        Assert.assertTrue(new JiraAnnotation(method).isIssuePresent());
+        Assert.assertEquals(new JiraAnnotation(method).getProject(), "HW");
+        Assert.assertEquals(new JiraAnnotation(method).getIssue(), "HW-123456");
+    }
+}
