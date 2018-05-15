adfc1d08be9497e289d43ee8b367e5b19463216e
Max Saperstone <max.saperstone@coveros.com>
2018-05-15 10:01:03 -0400

    Adding javadocs and licensing

--- src/main/java/com/coveros/selenified/utilities/Point.java
+++ src/main/java/com/coveros/selenified/utilities/Point.java
@@ -1,9 +1,47 @@
+/*
+ * Copyright 2017 Coveros, Inc.
+ *
+ * This file is part of Selenified.
+ *
+ * Selenified is licensed under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.coveros.selenified.utilities;
 
+/**
+ * A generic representation of a point, with two objects, X and Y. These
+ * could be interpreted as Integers, Doubles, or truly anything depending
+ * on the future needs.
+ *
+ * @param <X>
+ * @param <Y>
+ * @author Max Saperstone
+ * @version 3.0.2
+ * @lastupdate 5/15/2018
+ */
 public class Point<X, Y> {
     private X x;
     private Y y;
 
+    /**
+     * The generic constructor of the point representation, providing an
+     * initial X and Y coordinate in some form.
+     *
+     * @param x - the X coordinate of the point
+     * @param y - the Y coordinate of the point
+     */
     public Point(X x, Y y) {
         this.x = x;
         this.y = y;
