a2d97f52ac6c1799ecf46d129850eb6a5096b3fa
Max Saperstone <max.saperstone@coveros.com>
2019-06-27 22:31:52 -0400

    Adding in docs

--- src/main/java/com/coveros/selenified/utilities/ReportOverview.java
+++ src/main/java/com/coveros/selenified/utilities/ReportOverview.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2019 Coveros, Inc.
+ *
+ * This file is part of Selenified.
+ *
+ * Selenified is licensed under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package com.coveros.selenified.utilities;
 
 import com.coveros.selenified.Browser;
@@ -20,6 +40,14 @@ import static com.coveros.selenified.utilities.Property.BROWSER;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.newBufferedWriter;
 
+/**
+ * Creates a custom report at the conclusion of the test run, which nicely lays
+ * out the high level results, and links to all of the detailed reports
+ *
+ * @author Max Saperstone
+ * @version 3.2.0
+ * @lastupdate 6/28/2019
+ */
 public class ReportOverview extends EmailableReporter2 {
 
     private static final Logger log = Logger.getLogger(ReportOverview.class);
