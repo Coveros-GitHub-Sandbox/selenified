0ac7f541df3b415033d28c2cfb097ded695d51d8
Max Saperstone <max.saperstone@coveros.com>
2019-09-13 13:14:32 -0400

    Fixing imports

--- src/test/java/unit/CapabilitiesTest.java
+++ src/test/java/unit/CapabilitiesTest.java
@@ -11,6 +11,11 @@ import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
+import java.util.logging.Level;
+
+import static com.coveros.selenified.utilities.Property.HUB;
+import static com.coveros.selenified.utilities.Property.PROXY;
+import static com.coveros.selenified.utilities.Reporter.ENABLED_LOGS;
 import static org.testng.Assert.*;
 
 public class CapabilitiesTest extends SaveProperties {
