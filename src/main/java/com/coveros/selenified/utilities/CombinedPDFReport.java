572edc24078042bcc500220f5ec14a6194e6fd48
Matt Grasberger <grasbergerm@gmail.com>
2019-12-30 20:25:01 -0500

    refactor for all test suites

--- src/main/java/com/coveros/selenified/utilities/CombinedPDFReport.java
+++ src/main/java/com/coveros/selenified/utilities/CombinedPDFReport.java
@@ -24,76 +24,97 @@ import org.testng.xml.XmlSuite;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CombinedPDFReport implements IReporter {
 
     static final float INCH = 72;
-    static final String FILE_NAME= "/AllPDFReports.pdf";
+    static final String FILE_NAME = "/AllPDFReports.pdf";
     private static final Logger log = Logger.getLogger(CombinedPDFReport.class);
 
     @Override
     public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
         if (Property.generatePDF()) {
-            for (ISuite iSuite : suites) {
-                try {
-                    String testReportDirectory = iSuite.getOutputDirectory();
-                    List<ITestNGMethod> iTestNGMethods = iSuite.getAllMethods();
-                    PDFMergerUtility pdfMerger = new PDFMergerUtility();
-                    pdfMerger.setDestinationFileName(testReportDirectory + FILE_NAME);
-                    List<File> testReports = new ArrayList<>();
-                    for (ITestNGMethod iTestNGMethod : iTestNGMethods) {
-                        File testReport = new File(testReportDirectory, iTestNGMethod.getTestClass().getName() + "." + iTestNGMethod.getMethodName() + ".pdf");
-                        testReports.add(testReport);
-                        pdfMerger.addSource(testReport);
-                    }
-
-                    pdfMerger.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
-                    PDDocument document = PDDocument.load(new File(testReportDirectory + FILE_NAME));
-                    PDPage tableOfContents;
-                    // Create larger page size if there are more than 48 tests
-                    if (suites.get(0).getAllMethods().size() > 48) {
-                        tableOfContents = new PDPage(new PDRectangle(1000, testReports.size() * 17f));
-                    } else {
-                        tableOfContents = new PDPage();
-                    }
-
-                    float ph = tableOfContents.getMediaBox().getUpperRightY();
-
-                    PDFont font = PDType1Font.HELVETICA_BOLD;
-                    int fontSize = 12;
-                    PDPageContentStream contents = new PDPageContentStream(document, tableOfContents);
-                    contents.beginText();
-                    contents.setFont(font, fontSize);
-                    contents.newLineAtOffset(INCH, ph - INCH - fontSize);
-                    for (File file : testReports) {
-                        contents.showText(file.getName());
-                        contents.newLineAtOffset(0, -15);
-                    }
-                    contents.endText();
-                    contents.close();
-
-                    // Now add the link annotation, so the click on "Jump to page three" works
-                    for (int reportIndex = 1; reportIndex <= testReports.size(); reportIndex++) {
-                        int pageIndex = reportIndex - 1;
-                        PDAnnotationLink pageLink = createLink(ph, font, fontSize, "Go to " + testReports.get(pageIndex), reportIndex);
-                        addLink(document, tableOfContents, pageLink, pageIndex);
-                    }
-
-                    try (PDDocument newDoc = new PDDocument()) {
-                        PDPageTree allPages = document.getDocumentCatalog().getPages();
-
-                        allPages.insertBefore(tableOfContents, allPages.get(0));
-                        for (PDPage page : allPages) {
-                            newDoc.addPage(page);
-                        }
-                        newDoc.save(testReportDirectory + FILE_NAME);
-                    }
-                } catch (Exception e) {
-                    log.debug(e);
+            Map<String, List<ITestNGMethod>> testReportsMap = getTestSuiteMap(suites);
+            List<File> testReportsList = getAllTestReportFiles(testReportsMap);
+            try {
+                PDFMergerUtility pdfMerger = new PDFMergerUtility();
+                pdfMerger.setDestinationFileName(outputDirectory + FILE_NAME);
+                for (File testReport : testReportsList) {
+                    pdfMerger.addSource(testReport);
                 }
+
+                pdfMerger.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
+                PDDocument document = PDDocument.load(new File(outputDirectory + FILE_NAME));
+                PDPage tableOfContents;
+                // Create larger page size if there are more than 48 tests
+                if (suites.get(0).getAllMethods().size() > 48) {
+                    tableOfContents = new PDPage(new PDRectangle(1000, testReportsList.size() * 17f));
+                } else {
+                    tableOfContents = new PDPage();
+                }
+
+                float ph = tableOfContents.getMediaBox().getUpperRightY();
+
+                PDFont font = PDType1Font.HELVETICA_BOLD;
+                int fontSize = 12;
+                PDPageContentStream contents = new PDPageContentStream(document, tableOfContents);
+                contents.beginText();
+                contents.setFont(font, fontSize);
+                contents.newLineAtOffset(INCH, ph - INCH - fontSize);
+                for (File file : testReportsList) {
+                    contents.showText(file.getName());
+                    contents.newLineAtOffset(0, -15);
+                }
+                contents.endText();
+                contents.close();
+
+                // Now add the link annotation, so the click on "Jump to page three" works
+                for (int reportIndex = 1; reportIndex <= testReportsList.size(); reportIndex++) {
+                    int pageIndex = reportIndex - 1;
+                    PDAnnotationLink pageLink = createLink(ph, font, fontSize,
+                            "Go to " + testReportsList.get(pageIndex), reportIndex);
+                    addLink(document, tableOfContents, pageLink, pageIndex);
+                }
+
+                try (PDDocument newDoc = new PDDocument()) {
+                    PDPageTree allPages = document.getDocumentCatalog().getPages();
+
+                    allPages.insertBefore(tableOfContents, allPages.get(0));
+                    for (PDPage page : allPages) {
+                        newDoc.addPage(page);
+                    }
+                    newDoc.save(outputDirectory + FILE_NAME);
+                }
+            } catch (Exception e) {
+                log.debug(e);
             }
         }
+    }
+
+    private Map<String, List<ITestNGMethod>> getTestSuiteMap(List<ISuite> suites) {
+        Map<String, List<ITestNGMethod>> testReportsMap = new HashMap<>();
+        for (ISuite iSuite : suites) {
+            String testReportDirectory = iSuite.getOutputDirectory();
+            List<ITestNGMethod> iTestNGMethods = iSuite.getAllMethods();
+            testReportsMap.put(testReportDirectory, iTestNGMethods);
+        }
+        return testReportsMap;
+    }
+
+    private List<File> getAllTestReportFiles(Map<String, List<ITestNGMethod>> testReportsMap) {
+        List<File> testReportsList = new ArrayList<>();
+        for (Map.Entry<String, List<ITestNGMethod>> testReportsMapEntry : testReportsMap.entrySet()) {
+            String testReportDirectory = testReportsMapEntry.getKey();
+            for (ITestNGMethod iTestNGMethod : testReportsMapEntry.getValue()) {
+                File testReport = new File(testReportDirectory,
+                        iTestNGMethod.getTestClass().getName() + "." + iTestNGMethod.getMethodName() + ".pdf");
+                testReportsList.add(testReport);
+            }
+        }
+        return testReportsList;
     }
 
     private void addLink(PDDocument document, PDPage tableOfContents, PDAnnotationLink pageLink, int pageIndex) throws IOException {
